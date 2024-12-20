24eeacdde0e7cd04d60821b18cd7d0f491e9b25a
Robert Powell <repowell1@crimson.ua.edu>
2024-12-19 19:10:26 -0500

    First production version!

--- Python Source/UncheckRechecker.py
+++ Python Source/UncheckRechecker.py
@@ -40,13 +40,15 @@ def projectNumberToProjectID(projectNumber):
     }
 
     parameters = {
-        'Sort':"DateUpdated"
+        'Sort':"DateUpdated desc",
+        "pageSize":1000
     }
 
     # Find Project ID from Project Number (GET Project)
     response = requests.request("GET", "https://api.avaza.com/api/Project", headers=headers, params=parameters)
 
     json_data = response.text 
+    print(json_data)
 
     # Parse the JSON string
     parsed_data = json.loads(json_data)
@@ -92,25 +94,24 @@ def fixTaskCompletionOrder(projectID):
          
     #find "Section 1 Group"
     for group in projectViewData['groups']:
-        if "Section 1" in group['Title']:
-            for task in group["tasks"]:
-                model = { 
-                        "TaskID": f"{task["TaskID"]}",
-                        "FieldsToUpdate": ["TaskStatusCode"],
-                        "TaskStatusCode": "NotStarted",
-                    }
-                response = requests.request("PUT", "https://api.avaza.com/api/Task", headers=headers, json=model)
-                if (response.status_code != 200 and (statusCode == 0 or statusCode == 2)):
-                    statusCode+= 1
-                model = { 
-                        "TaskID": f"{task["TaskID"]}",
-                        "FieldsToUpdate": ["TaskStatusCode"],
-                        "TaskStatusCode": "Complete",
-                    }
-                response = requests.request("PUT", "https://api.avaza.com/api/Task", headers=headers, json=model)
-                if (response.status_code != 200 and (statusCode == 0 or statusCode == 2)):
-                    statusCode+= 1
-            break
+        for task in group["tasks"]:
+            print(f"editing Task {task["Title"]}")
+            model = { 
+                    "TaskID": f"{task["TaskID"]}",
+                    "FieldsToUpdate": ["TaskStatusCode"],
+                    "TaskStatusCode": "NotStarted",
+                }
+            response = requests.request("PUT", "https://api.avaza.com/api/Task", headers=headers, json=model)
+            if (response.status_code != 200 and (statusCode == 0 or statusCode == 2)):
+                statusCode+= 1
+            model = { 
+                    "TaskID": f"{task["TaskID"]}",
+                    "FieldsToUpdate": ["TaskStatusCode"],
+                    "TaskStatusCode": "Complete",
+                }
+            response = requests.request("PUT", "https://api.avaza.com/api/Task", headers=headers, json=model)
+            if (response.status_code != 200 and (statusCode == 0 or statusCode == 2)):
+                statusCode+= 1
     return statusCode
 
 
--- Python Source/dist/README.md
+++ Python Source/dist/README.md
@@ -10,6 +10,6 @@ Date: 12/19/2024
 2. Set the top line of `appsettings.json` to the desired mode.
     -  `"mode":"test",` for testing
     -  `"mode":"p2p",` for production
-    -  By default it is `"mode":"test",` for safety
+    -  By default it is `"mode":"p2p",`
 3. Run `TaskCompletionFixer.exe`, and enter the Project that needs to be invoiced
 4. Done! The project will now invoice in the correct order!
